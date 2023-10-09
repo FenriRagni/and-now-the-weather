cf2748121626995e66d809928885a3bd9adab08a
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-09 07:20:55 -0400

    add setWeather function to set today's weather

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
     var geoQuery = "http://api.openweathermap.org/geo/1.0/direct?q="
     var subBtn = $("#submit");
     var srchInput = $("#search");
-    function checkCoord(str){
+    var today = $("#today");
+    var date = dayjs();
+    //function to set coordinates in correct format
+    function setCoord(str){
         str = str.toString();
         if(str[0]==="-"){
             str = str.substring(0,6);
@@ -15,6 +18,21 @@ $(document).ready(function() {
         return str;
     }
     
+    //sets appropriate icon in url
+    function setIcon(icon){
+        return "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    }
+    //sets Today's Weather
+    function setWeather(data){
+        today.text(data.name+ " " + date.format("M/D"));
+        console.log(today.children());
+        today.siblings().children().eq(0).html("Temperature: "+ Math.round(data.main.temp)+"&deg;F");
+        today.siblings().children().eq(1).text("Wind: " + data.wind.speed+ " mph");
+        today.siblings().children().eq(2).text("Humidity: " + data.main.humidity+ "%");
+    }
+
+    
+
     subBtn.on("click", function(event) {
         event.preventDefault();
         console.log("button clicked");
@@ -23,20 +41,30 @@ $(document).ready(function() {
         $.ajax({
             url: geoQuery + "atlanta&limit=5&appid=" + key
         }).then(function(data){
-            console.log(data);
-            console.log(data[0]);
+            console.log("data:", data);
+            console.log("data[0]:", data[0]);
             long = data[0].lon;
             lati = data[0].lat;
-            console.log(long);
-            console.log(lati);
-            long = checkCoord(long);
-            lati = checkCoord(lati);
+            console.log("longitude:", long);
+            console.log("latitude:", lati);
+            long = setCoord(long);
+            lati = setCoord(lati);
             $.ajax({
                 url: "https://api.openweathermap.org/data/2.5/weather?lat=" + lati
                 + "&lon=" + long + "&appid=" + key + "&units=imperial"
             }).then(function(data){
-                console.log(data);
+                console.log("data:", data);
+                console.log("current weather data.main.temp:", Math.round(data.main.temp));
+                setWeather(data);
             });
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + lati
+                + "&lon=" + long + "&appid=" + key + "&units=imperial"
+            }).then(function(data){
+                console.log("forecast data:", data);
+                console.log("forcast data.list[2].main.temp:", Math.round(data.list[2].main.temp));
+
+            })
         });
     });
 
